80f16996192797f6a0e9d8639ab5c87f075c4d3c
nichengfuben <nichengfuben@outlook.com>
2026-07-14 21:36:37 +0800

    init: Provider-Azuretranslate-Adapter

---
+++ provider_azuretranslate/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+# src/platforms/azuretranslate/__init__.py
+from provider_azuretranslate.adapter import Adapter, AzureTranslateAdapter
+
+__all__ = ["AzureTranslateAdapter", "Adapter"]
